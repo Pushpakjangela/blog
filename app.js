e6192e45f75976f4024892c561012719069df779
Pushpakjangela <rv9109559291@gmail.com>
2024-01-22 01:50:03 +0530

    second commit

--- app.js
+++ app.js
@@ -139,11 +139,11 @@ app.get("/contact",function(req,res){
 });
 */
 
-let port = process.env.PORT;
-if (port == null || port == "") {
+let port = process.env.PORT||3000;
+/*if (port == null || port == "") {
   port = 3000;
-}
-app.listen(3000, function() {
+}*/
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
 
